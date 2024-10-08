a334196d728e184e651983d716ddbfdcb4ee396c
domenic21 <domenicarias21@gmail.com>
2024-10-08 12:11:36 -0400

    err

--- src/libs/session.ts
+++ src/libs/session.ts
@@ -25,7 +25,7 @@ export const session = nextAppSession<MySessionData>({
         });
       });
     },
-    set: (sid: string, session: string) => {
+    set: (sid: string, session: MySessionData | string) => {
       return new Promise((resolve, reject) => {
         MongoStore.create({
           mongoUrl: process.env.MONGODB_URI, // MongoDB connection string
