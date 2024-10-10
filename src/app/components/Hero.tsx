e0cb0686ee5c0080ae15121f799277ce3d4ea6a1
domenic21 <domenicarias21@gmail.com>
2024-10-10 13:06:42 -0400

    language correction

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -17,20 +17,19 @@ export default function Hero() {
         <div className="mx-auto max-w-full px-4 py-32 lg:flex lg:h-500 lg:items-center">
           <div className="mx-auto max-w-xl text-center ">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
-              Mejora tu Ingles YA! Consigue el {""}
+              Improve Your English NOW! Achieve {""}
               <span
-                className={
-                  "  text-yellow-400 cool-underline" +
-                  (showLine ? "show-underline" : "")
-                }
+              className={
+                "  text-yellow-400 cool-underline" +
+                (showLine ? "show-underline" : "")
+              }
               >
-                nivel C1/C2{" "}
+              C1/C2 level{" "}
               </span>
             </h1>
 
             <p className="mt-4 sm:text-xl/relaxed ">
-              Mejora tu Ingles con nosotros, tutores experimentados
-              profesionales.
+              Improve your English with us, experienced professional tutors.
             </p>
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
